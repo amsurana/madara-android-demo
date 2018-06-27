91201cff91ca07329ff89009e4e0e09488dedd74
Amit S <amit@smargav.com>
2018-06-27 20:49:57 +0530

    Updated the Demo App

--- app/src/main/java/ai/shield/shieldaidemo/MainActivity.java
+++ app/src/main/java/ai/shield/shieldaidemo/MainActivity.java
@@ -5,34 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import ai.shield.shieldaidemo.tests.TestKnowledgeBase;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    static {
-        System.loadLibrary("Madara_Jar");
-        System.loadLibrary("gams_jar");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-
-                    TestKnowledgeBase.main(null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-
     }
 
     public void listenForAgents(View view) {
