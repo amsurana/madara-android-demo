0fb8adafa3def909659f7111bba63220306dd505
Amit S <amit@smargav.com>
2018-06-29 00:29:29 +0530

    Updated demo with latest binaries

--- app/src/main/java/ai/shield/shieldaidemo/MainActivity.java
+++ app/src/main/java/ai/shield/shieldaidemo/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
 
 
@@ -13,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void listenForAgents(View view) {
